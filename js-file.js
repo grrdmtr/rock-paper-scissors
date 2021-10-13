a496843d241eaf301893cfc94c241f1bbd67af1a
grrdmtr <dumitrugraur@gmail.com>
2021-10-14 00:12:09 +0300

    Added css

--- js-file.js
+++ js-file.js
@@ -18,7 +18,7 @@ function playRound (playerSelection, computerSelection) {
     
     if (playerSelection == "ROCK" && computerSelection == "PAPER") {
          score = "lose";
-         result.textContent = "You Lose! PAPER beats ROCK";
+         result.textContent = "You Lose! paper beats rock";
     } else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
          score = "win";
          result.textContent = "You win!";
@@ -27,10 +27,10 @@ function playRound (playerSelection, computerSelection) {
          result.textContent = "You win!";
     } else if (playerSelection == "SCISSORS" && computerSelection == "ROCK") {
          score = "lose";
-         result.textContent = "You Lose! ROCK beats SCISSORS";
+         result.textContent = "You Lose! rock beats scissors";
     } else if (playerSelection == "PAPER" && computerSelection == "SCISSORS") {
          score = "lose";
-         result.textContent = 'You Lose! SCISSORS beats PAPER';
+         result.textContent = 'You Lose! scissors beats paper';
     } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
          score = "win";
          result.textContent = "You win!";
@@ -66,22 +66,41 @@ function checkWinner() {
     totalScore = playerScore + computerScore;    
 
      if (playerScore > computerScore && totalScore === 5) {
+        scoreContainer.textContent = `The score is ${playerScore} - ${computerScore}`; 
         result.textContext = "Congratulations! You won!";
         playerScore = 0;
         computerScore = 0;
      } else if (playerScore < computerScore &&  totalScore === 5) {
-        result.textContent = "Bohoo! You lose!";
+        scoreContainer.textContent = `The score is ${playerScore} - ${computerScore}`; 
+        result.textContent = "Bohoo! You lost!";
         playerScore = 0;
         computerScore = 0;
      } else if (playerScore == computerScore &&  totalScore === 5) {
+        scoreContainer.textContent = `The score is ${playerScore} - ${computerScore}`; 
         result.textContext = "Ha! It's a draw!";
         playerScore = 0;
         computerScore = 0;
+     } else {
+         scoreContainer.textContent = `The score is ${playerScore} - ${computerScore}`;
      }
 }
 
- const container = document.querySelector('#container');
+ const gameContainer = document.querySelector('#game')
+
+ const results = document.createElement('div');
+ results.classList.add('results');
+ gameContainer.appendChild(results);
+
+ const title = document.createElement('div');
+ title.textContent = "SCORE BOARD";
+ results.appendChild(title);
 
  const result = document.createElement('div');
+
  result.classList.add('result');
- container.appendChild(result);
+ results.appendChild(result);
+
+ const scoreContainer = document.createElement('div');
+
+ scoreContainer.classList.add('scoreContainer');
+ results.appendChild(scoreContainer);
